07d077ec299893597fb227a064cfe64b96e59dab
BytingBulldogs3539 <bbprogrammers3539@gmail.com>
2024-01-13 14:06:53 -0500

    chmod take 2

--- install_limelight3.sh
+++ install_limelight3.sh
@@ -1,6 +1,5 @@
 # Run the pi install script
-#chmod +x install_pi.sh
-#./install_pi.sh
+./install_pi.sh
 
 # Add the one extra file for the LL3
 wget https://datasheets.raspberrypi.org/cmio/dt-blob-cam1.bin -O /boot/dt-blob.bin
