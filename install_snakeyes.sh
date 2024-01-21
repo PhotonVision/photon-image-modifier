61d8edd67a94cf5a0de0cab2f1414dd4b08d2def
Chris <chrisgerth010592@gmail.com>
2024-01-21 10:11:39 -0600

    missing directory

--- install_snakeyes.sh
+++ install_snakeyes.sh
@@ -2,4 +2,5 @@
 ./install_pi.sh
 
 # Add the one extra file for the snakeyes hardware config
+mkdir -p /opt/photonvision/photonvision_config
 install -v -m 644 files/snakeyes/photon.sqlite /opt/photonvision/photonvision_config/photon.sqlite
