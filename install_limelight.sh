d47dbee39d554389bba621a50cc953295710506f
Craig Schardt <crschardt@fastem.com>
2024-10-13 12:11:42 -0500

    ARCH may not be detected correctly in chroot, so specify it

--- install_limelight.sh
+++ install_limelight.sh
@@ -1,6 +1,6 @@
 # Run normal photon installer
 chmod +x ./install.sh
-./install.sh --install-nm=yes
+./install.sh --install-nm=yes --arch=aarch64
 
 # edit boot partition
 install -m 644 limelight/config.txt /boot/
