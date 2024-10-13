bb0f987de84f3b56b990cb5c8db37e7a4d6adf5f
Craig Schardt <crschardt@fastem.com>
2024-10-12 21:22:35 -0500

    fix wrong AllowedCPUs from main repo

--- install.sh
+++ install.sh
@@ -321,7 +321,7 @@ fi
 
 if [[ -n $(cat /proc/cpuinfo | grep "RK3588") ]]; then
   debug "This has a Rockchip RK3588, enabling all cores"
-  sed -i 's/# AllowedCPUs=4-7/AllowedCPUs=0-7/g' /lib/systemd/system/photonvision.service
+  sed -i 's/# AllowedCPUs=4-7/AllowedCPUs=4-7/g' /lib/systemd/system/photonvision.service
 fi
 
 cp /lib/systemd/system/photonvision.service /etc/systemd/system/photonvision.service
