ff5f5e62a8d99142e4fc302bac01dcaaa5cff5af
Craig Schardt <crschardt@fastem.com>
2025-01-12 16:45:53 -0600

    same line in multiple install scripts

--- install_dev_pi.sh
+++ install_dev_pi.sh
@@ -11,7 +11,7 @@ install -m 644 config.txt /boot/
 install -m 644 userconf.txt /boot/
 
 # Kill wifi and other networking things
-install -v -m 644 files/wait.conf /etc/systemd/system/dhcpcd.service.d/
+install -v -m 644 -D -t /etc/systemd/system/dhcpcd.service.d/ files/wait.conf
 install -v files/rpi-blacklist.conf /etc/modprobe.d/blacklist.conf
 
 # Update pigipio service file to listen locally
