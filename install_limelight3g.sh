19bc3082194426e644ea447eef2bdd0720c2b6be
Jade Turner <spacey-sooty@proton.me>
2025-05-09 11:14:20 +0800

    fix
    Signed-off-by: Jade Turner <spacey-sooty@proton.me>

--- install_limelight3g.sh
+++ install_limelight3g.sh
@@ -8,7 +8,7 @@ chmod +x ./install_pi.sh
 ./install_pi.sh
 
 # Install our new config.txt with OV9281 overlay
-install -m 644 limelight4/config.txt /boot/
+install -m 644 limelight3g/config.txt /boot/
 
 # Add the one extra file for the LL3
 wget https://datasheets.raspberrypi.org/cmio/dt-blob-cam1.bin -O /boot/dt-blob.bin
