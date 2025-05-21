27978fd29d8a0ebf8525cd914f9467f589387703
Avery Black <avery.c.black@gmail.com>
2025-05-21 16:26:23 -0700

    Update install_rubik.sh

--- install_rubik.sh
+++ install_rubik.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -v
 
+apt remove gnome
+apt autoremove
+apt remove cups firefox-esr gstreamer1.0-plugins-base xwayland xorg
+apt autoremove
+apt remove pipewire pulseaudio
+apt autoremove
+
+apt remove wpasupplicant
+
+rm -r /usr/share/gstreamer-1.0 
+rm -r /usr/lib/firmware/qcacld/
+rm -r /usr/lib/modules/6.6.38/kernel/drivers/net/wireless
+
+# rm -r /var/cache/apt
