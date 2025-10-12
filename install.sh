88425322ad472692a1e74acb10f8781d41c7463c
samfreund <samf.236@proton.me>
2025-10-11 20:04:01 -0500

    remove jammy apt source

--- install.sh
+++ install.sh
@@ -286,16 +286,16 @@ fi
 
 debug ""
 debug "Installing additional math packages"
-if [[ "$DISTRO" = "Ubuntu" && -z $(apt-cache search libcholmod3) ]]; then
-  debug "Adding jammy to list of apt sources"
-  if [[ -z $TEST ]]; then
-    if [[ "$ARCH" = "x86_64" ]]; then
-      add-apt-repository -y -S 'deb http://security.ubuntu.com/ubuntu jammy main universe'
-    else 
-      add-apt-repository -y -S 'deb http://ports.ubuntu.com/ubuntu-ports jammy main universe'
-    fi
-  fi
-fi
+# if [[ "$DISTRO" = "Ubuntu" && -z $(apt-cache search libcholmod3) ]]; then
+#   debug "Adding jammy to list of apt sources"
+#   if [[ -z $TEST ]]; then
+#     if [[ "$ARCH" = "x86_64" ]]; then
+#       add-apt-repository -y -S 'deb http://security.ubuntu.com/ubuntu jammy main universe'
+#     else 
+#       add-apt-repository -y -S 'deb http://ports.ubuntu.com/ubuntu-ports jammy main universe'
+#     fi
+#   fi
+# fi
 
 install_if_missing libcholmod3
 install_if_missing liblapack3
