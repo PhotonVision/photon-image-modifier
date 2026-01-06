df76e3e1afdd2b5d6747d06d4662bab3b2f11a85
Craig Schardt <crschardt@fastem.com>
2026-01-05 22:00:08 -0600

    fix double mounting

--- install_limelight3.sh
+++ install_limelight3.sh
@@ -3,14 +3,14 @@
 # Exit on errors, print commands, ignore unset variables
 set -ex +u
 
+# Run the pi install script
+chmod +x ./install_pi.sh
+./install_pi.sh
+
 # mount partition 1 as /boot/firmware
 mkdir --parent /boot/firmware
 mount "${loopdev}p1" /boot/firmware
 ls -la /boot/firmware
-
-# Run the pi install script
-chmod +x ./install_pi.sh
-./install_pi.sh
 
 # Add the one extra file for the LL3
 wget https://datasheets.raspberrypi.org/cmio/dt-blob-cam1.bin -O /boot/firmware/dt-blob.bin
--- install_luma_p1.sh
+++ install_luma_p1.sh
@@ -3,14 +3,14 @@
 # Exit on errors, print commands, ignore unset variables
 set -ex +u
 
+# Run the pi install script
+chmod +x ./install_pi.sh
+./install_pi.sh
+
 # mount partition 1 as /boot/firmware
 mkdir --parent /boot/firmware
 mount "${loopdev}p1" /boot/firmware
 ls -la /boot/firmware
-
-# Run the pi install script
-chmod +x ./install_pi.sh
-./install_pi.sh
 
 # Install our new config.txt with OV9281 overlay
 install -m 644 luma_p1/config.txt /boot/firmware
