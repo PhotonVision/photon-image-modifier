8e3337b1a9dfa846e453bc2d109b62d356a32245
Craig Schardt <crschardt@fastem.com>
2026-01-05 21:54:51 -0600

    update the raspberry pi sctipts

--- install_limelight3g.sh
+++ install_limelight3g.sh
@@ -3,12 +3,19 @@
 # Exit on errors, print commands, ignore unset variables
 set -ex +u
 
+# mount partition 1 as /boot/firmware
+mkdir --parent /boot/firmware
+mount "${loopdev}p1" /boot/firmware
+ls -la /boot/firmware
+
 # Run the pi install script
 chmod +x ./install_pi.sh
 ./install_pi.sh
 
 # Install our new config.txt with OV9281 overlay
-install -m 644 limelight3g/config.txt /boot/
+install -m 644 limelight3g/config.txt /boot/firmware
 
 # Add the one extra file for the LL3
-wget https://datasheets.raspberrypi.org/cmio/dt-blob-cam1.bin -O /boot/dt-blob.bin
+wget https://datasheets.raspberrypi.org/cmio/dt-blob-cam1.bin -O /boot/firmware/dt-blob.bin
+
+umount /boot/firmware
