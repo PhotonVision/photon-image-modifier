4d3b8cb7358104a0c4a01410c983f9f91a8304e3
samfreund <samf.236@proton.me>
2026-04-04 00:12:14 -0500

    copy stuff over

--- install_limelight3g.sh
+++ install_limelight3g.sh
@@ -15,4 +15,6 @@ ls -la /boot/firmware
 # Install our new config.txt with OV9281 overlay
 install -m 644 limelight3g/config.txt /boot/firmware
 
+cp /limelight3/dt-blob.bin /boot/firmware/dt-blob.bin
+
 umount /boot/firmware
