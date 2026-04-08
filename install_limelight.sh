2864e82d54a2d9b6998a788d644f902c03d9fe6a
samfreund <samf.236@proton.me>
2026-04-08 17:39:14 -0500

    parameterize

--- install_limelight.sh
+++ install_limelight.sh
@@ -5,15 +5,12 @@ set -ex +u
 
 # Run the pi install script
 chmod +x ./install_pi.sh
-./install_pi.sh "$1"
+./install_pi.sh "$1" "limelight/config.txt"
 
 # mount partition 1 as /boot/firmware
 mkdir --parent /boot/firmware
 mount "${loopdev}p1" /boot/firmware
 ls -la /boot/firmware
-
-# Install our new config.txt with pwm overlay and ethernet config
-install -m 644 limelight/config.txt /boot/firmware/
 
 # link old config.txt location for diozero compatibility
 # TODO(thatcomputerguy0101): Remove this when diozero checks the new location
