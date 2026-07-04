8434f0ecd4bcbc0f0db3d5281f179628548113bd
Craig Schardt <crschardt@fastem.com>
2026-07-03 23:06:13 -0500

    it's re-enabled automatically

--- install_opi5_armbian.sh
+++ install_opi5_armbian.sh
@@ -36,7 +36,7 @@ sed -i s/extraargs=/#extraargs=/g /boot/armbianEnv.txt
 echo "extraargs=cma=256M initcall_debug ignore_loglevel" >> /boot/armbianEnv.txt
 
 # networkd isn't being used, this causes an unnecessary delay
-systemctl disable systemd-networkd-wait-online.service
+# systemctl disable systemd-networkd-wait-online.service
 
 # PhotonVision server is managing the network, so it doesn't need to wait for online
 systemctl disable NetworkManager-wait-online.service
